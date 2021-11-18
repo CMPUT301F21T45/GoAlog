21e62307268b173c1e43e6f560058b78caadd644
OctoLuis <73768287+OctoLuis@users.noreply.github.com>
2021-11-17 19:10:31 -0700

    User Class draft

--- app/src/main/java/com/example/goalog/User.java
+++ app/src/main/java/com/example/goalog/User.java
@@ -1,6 +1,42 @@
 package com.example.goalog;
 
+import java.util.ArrayList;
+
 public class User {
-    private String privateName;
+    private String loginName;
     private String publicName;
+    private String password;
+    private ArrayList<Habit> habits;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPublicName() {
+        return publicName;
+    }
+
+    public void setPublicName(String publicName) {
+        this.publicName = publicName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Habit> getHabits() {
+        return habits;
+    }
+
+    public void setHabits(ArrayList<Habit> habits) {
+        this.habits = habits;
+    }
 }
