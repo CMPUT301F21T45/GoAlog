74c3b3b5bc6352de8c3ab75a82ef17c3eba9fae6
Leitala <yhe7@ualberta.ca>
2021-11-23 17:28:13 -0700

    Update camera

--- app/src/main/java/com/example/goalog/AddHabitEventActivity.java
+++ app/src/main/java/com/example/goalog/AddHabitEventActivity.java
@@ -1,11 +1,19 @@
 package com.example.goalog;
 
+import static android.content.ContentValues.TAG;
+
+import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -18,16 +26,31 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -51,9 +74,15 @@ public class AddHabitEventActivity extends AppCompatActivity {
     private Button confirmButton;
     private String eventCommentString;
     private String completeDate;
+    private String imgPath = "";
+    private String currentPhotoPath;
     private TextView title;
     private TextView dateComplete;
     private Uri filePath;
+    private Uri photoURI;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     // check whether the Activity is used for add or edit a HabitEvent
     private boolean editMode = false;
@@ -108,11 +137,6 @@ public class AddHabitEventActivity extends AppCompatActivity {
         title.setText(clickedHabit.getHabitTitle());
         dateComplete.setText(completeDate);
 
-        final FirebaseFirestore database = FirebaseFirestore.getInstance();
-        final CollectionReference collectionReference = database.collection("user003");
-        FirebaseStorage storage;
-        StorageReference storageReference;
-
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
 
@@ -143,9 +167,13 @@ public class AddHabitEventActivity extends AppCompatActivity {
                     if (filePath != null) {
                         StorageReference ref = storageReference.child("images/" + UUID.randomUUID().toString());
                         ref.putFile(filePath);
+                        imgPath = filePath.toString();
                     }
+
+
                     final String habitEventID = UUID.randomUUID().toString().replace("-", "");
-                    HabitEvent newHabitEvent = new HabitEvent(habitEventID, eventCommentString, completeDate, clickedHabit.getHabitTitle(), filePath.toString());
+
+                    HabitEvent newHabitEvent = new HabitEvent(habitEventID, eventCommentString, completeDate, clickedHabit.getHabitTitle(), imgPath);
                     HashMap<String, HabitEvent> data = new HashMap<>();
                     data.put("Event", newHabitEvent);
                     if (newHabitEvent != null) {
@@ -156,12 +184,13 @@ public class AddHabitEventActivity extends AppCompatActivity {
                     Intent intent = new Intent(AddHabitEventActivity.this, UserPageActivity.class);
                     Toast.makeText(v.getContext(), "New Event Created", Toast.LENGTH_SHORT).show();
                     startActivity(intent);
-                    }
                 }
+            }
 
         });
     }
-    private void setChoosePhoto(){
+
+    private void setChoosePhoto() {
         View chooseTypeView = LayoutInflater.from(this).inflate(R.layout.choose_type_view, null);
         AlertDialog selection = new AlertDialog.Builder(this).setView(chooseTypeView).setCancelable(false).create();
         selection.show();
@@ -198,9 +227,25 @@ public class AddHabitEventActivity extends AppCompatActivity {
     // open phone camera
 
     private void openCamera() {
-        //File imgDir = new File(getFilePath(null));
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        openCameraResultLauncher.launch(takePictureIntent);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.e(TAG, "Fail to capture");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                openCameraResultLauncher.launch(takePictureIntent);
+            }
+
+        }
     }
 
     ActivityResultLauncher<Intent> openCameraResultLauncher = registerForActivityResult(
@@ -210,7 +255,8 @@ public class AddHabitEventActivity extends AppCompatActivity {
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() != RESULT_CANCELED) {
                         if (result.getResultCode() == RESULT_OK) {
-                            filePath = result.getData().getData();
+                            filePath = photoURI;
+                            galleryAddPic(filePath);
                             Bundle extras = result.getData().getExtras();
                             Bitmap imageBitmap = (Bitmap) extras.get("data");
                             image.setImageBitmap(imageBitmap);//Display image on screen
@@ -219,6 +265,7 @@ public class AddHabitEventActivity extends AppCompatActivity {
                     }
                 }
             });
+
     private void openGallery() {
         Intent photoPicker = new Intent(Intent.ACTION_PICK);
         photoPicker.setType("image/*");
@@ -239,26 +286,62 @@ public class AddHabitEventActivity extends AppCompatActivity {
                     }
                 }
             });
+
+    /*
+     * save picture to gallery
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic(Uri photoPath) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    public void saveImageSd(Bitmap bitmap, String fileName) {
+        FileOutputStream fos = null;
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            String imagePath = Environment.getExternalStorageDirectory().getPath() + "/service";
+            try {
+                File file = new File(imagePath, fileName + ".jpg");
+                System.out.println(file.getPath());
+                fos = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.flush();
+                Toast.makeText(this, "图片保存成功" + imagePath, Toast.LENGTH_SHORT).show();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri uri = Uri.parse("file://" + imagePath + fileName);
+            intent.setData(uri);
+            this.sendBroadcast(intent);
+        }
+    }
 }
 
 
 
-/*
 
-    public String getFilePath(String dir){
-        String path;
-        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
-            path = getExternalFilesDir(dir).getAbsolutePath();
-        } else{
-            path = getFilesDir() + File.separator +dir;
-        }
-        File file = new File(path);
-        if (!file.exists()){
-            file.mkdir();
-        }
-        return path;
-    }
-*/
 
 
 
